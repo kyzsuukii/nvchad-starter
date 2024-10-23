692fa82982c22ffd0bf9cd109baf4bbdaaaf0042
Dwiky Rizky Ananditya <kyzsuki@gmail.com>
2024-10-24 01:55:51 +0700

    intial config updates

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,63 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+local chat = require "CopilotChat"
+local hop = require "hop"
+local directions = require("hop.hint").HintDirection
+
+map("n", "<A-Up>", ":m .-2<CR>==")
+map("i", "<A-Up>", "<Esc>:m .-2<CR>==gi")
+map("v", "<A-Up>", ":m '<-2<CR>gv=gv")
+
+map("n", "<A-Down>", ":m .+1<CR>==")
+map("i", "<A-Down>", "<Esc>:m .+1<CR>==gi")
+map("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+
+map("n", "<A-S-Up>", "yyp")
+map("i", "<A-S-Up>", "<Esc>yypgi")
+map("v", "<A-S-Up>", "y`>pgv")
+
+map("n", "<A-S-Down>", "yyP")
+map("i", "<A-S-Down>", "<Esc>yyPgi")
+map("v", "<A-S-Down>", "y`<Pgv")
+
+map("n", "<C-a>", "ggVG")
+map("i", "<C-a>", "<Esc>ggVG")
+map("v", "<C-a>", "ggVG")
+
+map("n", "<leader>ct", function()
+  chat.toggle()
+end)
+
+map("n", "<leader>co", function()
+  chat.open()
+end)
+
+map("n", "<leader>cc", function()
+  chat.close()
+end)
+
+map("n", "<leader>cr", function()
+  chat.reset()
+end)
+
+map("n", "f", function()
+  hop.hint_words()
+end)
+
+map("n", "fb", function()
+  hop.hint_words { direction = directions.BEFORE_CURSOR }
+end)
+
+map("n", "fa", function()
+  hop.hint_words { direction = directions.AFTER_CURSOR }
+end)
+
+map("n", "fl", function()
+  hop.hint_lines()
+end)
+
+map("n", "f/", function()
+  hop.hint_patterns()
+end)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -22,4 +22,75 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      panel = {
+        auto_refresh = true,
+        layout = {
+          position = "right",
+          ratio = 0.3,
+        },
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
+    },
+  },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = false, -- Enable debugging
+    },
+  },
+
+  {
+    "smoka7/hop.nvim",
+    opts = {
+      keys = "etovxqpdygfblzhckisuran",
+    },
+  },
+
+  {
+    "LintaoAmons/cd-project.nvim",
+    init = function()
+      require("cd-project").setup {
+        projects_config_filepath = vim.fs.normalize(vim.fn.stdpath "config" .. "/cd-project.nvim.json"),
+        project_dir_pattern = { ".git", ".gitignore", "Cargo.toml", "package.json", "go.mod" },
+        choice_format = "both",
+        projects_picker = "telescope",
+        auto_register_project = false,
+        hooks = {
+          -- {
+          --   trigger_point = "BEFORE_CD",
+          --   callback = function(_)
+          --     vim.print("before cd project")
+          --     require("bookmarks").api.mark({name = "before cd project"})
+          --   end,
+          -- },
+          -- {
+          --   callback = function(_)
+          --     require("telescope").extensions.smart_open.smart_open({
+          --       cwd_only = true,
+          --       filename_first = false,
+          --     })
+          --   end,
+          -- },
+        },
+      }
+    end,
+  },
 }
