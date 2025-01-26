7618f22ece3bebfe62fb59830e917a17d39325a4
Dwiky Rizky Ananditya <kyzsuki@gmail.com>
2025-01-26 15:54:51 +0700

    update lspconfig
    Signed-off-by: Dwiky Rizky Ananditya <kyzsuki@gmail.com>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "jsonls", "tailwindcss", "prismals", "ts_ls", "intelephense" }
+local servers = { "jsonls", "ts_ls", "eslint", "phan" }
 
 local nvlsp = require "nvchad.configs.lspconfig"
 
@@ -23,3 +23,54 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+lspconfig.html.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "html", "php", "blade" },
+}
+
+lspconfig.cssls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    css = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    scss = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+  },
+}
+
+lspconfig.phpactor.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  cmd = { "phpactor", "language-server" },
+  filetypes = { "php", "blade" },
+}
+
+local tailwindcss_default_config = lspconfig.tailwindcss.document_config.default_config
+
+lspconfig.tailwindcss.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  filetypes = vim.tbl_extend("force", tailwindcss_default_config.filetypes, { "php", "blade" }),
+  settings = {
+    tailwindCSS = {
+      lint = {
+        invalidApply = false,
+      },
+    },
+  },
+}
